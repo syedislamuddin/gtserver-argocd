d0e61cb4181ee879e62ac37a624986de3d8a3fc2
dvitale199 <d.vitale199@gmail.com>
2025-03-26 09:02:51 -0600

    Squashed 'microservices/genotools_api/' content from commit 99cdc22
    git-subtree-dir: microservices/genotools_api
    git-subtree-split: 99cdc229c9cd9275d2153604bc9b2725048b85b7

---
+++ scripts/testing.sh
@@ -0,0 +1,6 @@
+uvicorn genotools_api.main:app --host 0.0.0.0 --port 8080 --reload
+
+
+
+
+# genotools --pfile /app/genotools_api/data/GP2_merge_AAPDGC --out /app/genotools_api/output/dan_test --ref_panel /app/genotools_api/data/1kg_30x_hgdp_ashk_ref_panel --ancestry --model /app/genotools_api/data/nba_v1.pkl --all_sample --all_variant 
